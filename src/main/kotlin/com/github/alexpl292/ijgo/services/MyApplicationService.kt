665e8890ca2a30c3d90f2e50059c46a491d6a15c
GitHub Action <action@github.com>
2022-02-03 08:34:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexpl292/ijgo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alexpl292/ijgo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexpl292.ijgo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexpl292.ijgo.MyBundle
 
 class MyApplicationService {
 
